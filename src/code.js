f9a4a5ed55e16a864f32b7d8be9c1f8afd2d10cd
amkera <amber_kerawala@berkeley.edu>
2021-02-18 20:17:06 -0500

    closure practice

--- src/code.js
+++ src/code.js
@@ -14,28 +14,41 @@
 //so the inner function has access to the outer function's variable, outerVariable, which is set to 'outside'
 
 
+//
+// let addTo = function(passed) {
+//   let inner = 2
+//   return passed + inner;
+// }
+//
+// //returns passed(3) added to inner(2)
+//
+// console.log(addTo(3)); // => 5
+//
+//
+// //in javascript, you don't need to pass in the passed variable. like so:
+//
+// let argument = 3;
+//
+// let add = function() {
+//   let inner = 2;
+//   return argument + inner
+//   //JS has to look outside the function to see what argument is
+// }
+//
+// argument = 4; //most recent version of variable will be used
+//
+// console.log(add())
+// //Variables outside the function are available inside the function due to lexical scoping.
 
-let addTo = function(passed) {
-  let inner = 2
-  return passed + inner;
+var addTo = function(passed) {
+
+  var add = function(inner) {
+    return passed + inner
+  }
+
+  return add;
 }
 
-//returns passed(3) added to inner(2)
-
-console.log(addTo(3)); // => 5
-
-
-//in javascript, you don't need to pass in the passed variable. like so:
-
-let argument = 3;
-
-let add = function() {
-  let inner = 2;
-  return argument + inner
-  //JS has to look outside the function to see what argument is
-}
-
-argument = 4; //most recent version of variable will be used
-
-console.log(add())
-//Variables outside the function are available inside the function due to lexical scoping.
+var addThree = new addTo(3);
+console.log(addThree) //this logs a function! once you pass it an argument, it will execute:
+console.log(addThree(1));
